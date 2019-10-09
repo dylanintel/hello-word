910dad4e4d7f7a3ff38bd89654f09d8e3a66b92c
Wang Dylan <dylan.wang@intel.com>
2019-10-09 09:37:27 +0800

    hello world code

---
+++ helloword.cpp
@@ -0,0 +1,15 @@
+/*
+ * @file: helloword.cpp
+ * @author: Zhu, Bin Qiang <bin.qiangx.zhu@intel.com>
+ * @create time: 2019-10-09 09:30:59
+ * @last modified: 2019-10-09 09:30:59
+ * @description:
+ */
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout<< "hello world !";
+    return 0;
+}
